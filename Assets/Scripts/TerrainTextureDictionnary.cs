94a46a5cf023f13e980242dff5d87c283b54a692
Samuel Therrien <samuel.06@hotmail.com>
2017-09-27 11:11:33 -0400

    Adapter le Physic Material des projectiles

--- Assets/Scripts/TerrainTextureDictionnary.cs
+++ Assets/Scripts/TerrainTextureDictionnary.cs
@@ -4,14 +4,14 @@
 public class TerrainTextureDictionnary : MonoBehaviour {
     public static string[] Textures { get; private set; }
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start () {
         Terrain terrain = Terrain.activeTerrain;
         TerrainData terrainData = terrain.terrainData;
         SplatPrototype[] splatPrototypes = terrainData.splatPrototypes;
         Textures = new string[splatPrototypes.Length];
         for (int i = 0; i < splatPrototypes.Length; i++) {
-            Textures[i] = splatPrototypes[i].texture.ToString();
+            Textures[i] = splatPrototypes[i].texture == null ? "null" : splatPrototypes[i].texture.name;
         }
     }
 
