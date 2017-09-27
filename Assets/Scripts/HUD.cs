f60953b407c9e75b4c49bf7e85a14abc73121825
Marcu101 <marcu101@hotmail.com>
2017-09-27 17:18:15 -0400

    Update du menu/HUD
    Changement des boutons du menu, ajout de boutons dans le HUD, commencement du script du HUD

---
+++ Assets/Scripts/HUD.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class HUD : MonoBehaviour {
+
+    public void menu_principal()
+    {
+        SceneManager.LoadScene(0); 
+    }
+
+    public void reset()
+    {
+        SceneManager.LoadScene(1);
+    }
+	
+}
