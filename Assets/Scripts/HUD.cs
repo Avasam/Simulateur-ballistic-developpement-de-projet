6d8631a3664e86b820610799faaab3169a8c388a
Marcu101 <marcu101@hotmail.com>
2017-10-05 13:29:39 -0400

    Menu aide
    ajout du menu d'aide pour les boutons du UI

--- Assets/Scripts/HUD.cs
+++ Assets/Scripts/HUD.cs
@@ -6,6 +6,16 @@ using UnityEngine.UI;
 
 public class HUD : MonoBehaviour {
 
+    public Canvas CanvasOptionsJeu;
+    public Canvas CanvasHUD;
+    public Canvas CanvasAide;
+
+    void Awake()
+    {
+        CanvasOptionsJeu.enabled = false;
+        CanvasAide.enabled = false;
+    }
+
     public void menu_principal()
     {
         SceneManager.LoadScene(0); 
@@ -15,5 +25,27 @@ public class HUD : MonoBehaviour {
     {
         SceneManager.LoadScene(1);
     }
-	
+        
+
+    public void OptionsMenu()
+    {
+        CanvasOptionsJeu.enabled = true;
+        CanvasHUD.enabled = false;
+
+    }
+
+    public void RetourMenu()
+    {
+        CanvasOptionsJeu.enabled = false;
+        CanvasAide.enabled = false;
+        CanvasHUD.enabled = true;
+
+    }
+
+    public void AideMenu()
+    {
+        CanvasHUD.enabled = false;
+        CanvasAide.enabled = true;
+    }
+
 }
