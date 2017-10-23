5dba580cea5eab8f4e582769d2eb7bf4512bcf2b
Samuel Therrien <samuel.06@hotmail.com>
2017-10-23 10:37:08 -0400

    Fixed/updated a bunch of small stuff
    - Removed duplicate audio listener
    - Removed empty folders
    - Baked all lightning
    - Added PhysicsWindZones in all scenes
    - Fixed Terrain Texture detection
    - Reduced water quality
    - Added collider and layer on new water

--- Assets/Scripts/ProjectilePhysics.cs
+++ Assets/Scripts/ProjectilePhysics.cs
@@ -6,16 +6,18 @@ public class ProjectilePhysics : MonoBehaviour {
     [SerializeField] int waterCollidingCount = 0;
     private SphereCollider sphereCollider;
     private Rigidbody rBody;
+    private int layerID;
 
 	// Use this for initialization
 	void Awake () {
         sphereCollider = gameObject.GetComponent<SphereCollider>();
         rBody = gameObject.GetComponent<Rigidbody>();
+        layerID = LayerMask.NameToLayer("Water");
     }
 
 
     private void OnTriggerEnter(Collider trigger) {
-        if (trigger.gameObject.layer == 4) {
+        if (trigger.gameObject.layer == layerID) {
             waterCollidingCount++;
             rBody.drag = 5;
         }
@@ -23,7 +25,7 @@ public class ProjectilePhysics : MonoBehaviour {
     }
 
     private void OnTriggerExit(Collider trigger) {
-        if (trigger.gameObject.layer == 4) {
+        if (trigger.gameObject.layer == layerID) {
             waterCollidingCount--;
             if (waterCollidingCount <= 0) {
                 rBody.drag = 0;
@@ -43,29 +45,33 @@ public class ProjectilePhysics : MonoBehaviour {
 
     private void AdaptPhysicMaterial(Collision collision) {
         if (collision.gameObject.GetComponent<Terrain>()) {
-            // Get the texture name
-            int surfaceIndex = TerrainSurface.GetMainTexture(transform.position);
-            string textureName = TerrainTextureDictionnary.Textures[surfaceIndex];
+            if (TerrainTextureDictionnary.Textures != null) {
+                // Get the texture name
+                int surfaceIndex = TerrainSurface.GetMainTexture(transform.position);
+                string textureName = TerrainTextureDictionnary.Textures[surfaceIndex];
 
-            // Find the appropriate material
-            if (textureName.Length >= 4) {
-                string physicMatName = textureName.Substring(0, 4);
-                PhysicMaterial physicMat = (PhysicMaterial)Resources.Load(physicMatName);
-                if (physicMat != null) {
-                    sphereCollider.material = physicMat;
-            
-            // Dealing with problems
-                } else {
-                    Debug.LogWarning("Can't find Physic Material called \"" + physicMatName + "\" for Texture \"" + textureName + "\". Default Material \"" + defaultMaterialName + "\" used instead.");
-                    physicMat = (PhysicMaterial)Resources.Load(defaultMaterialName);
+                // Find the appropriate material
+                if (textureName.Length >= 4) {
+                    string physicMatName = textureName.Substring(0, 4);
+                    PhysicMaterial physicMat = (PhysicMaterial)Resources.Load(physicMatName);
                     if (physicMat != null) {
-                        sphereCollider.material = sphereCollider.material;
+                        sphereCollider.material = physicMat;
+
+              // Dealing with problems
                     } else {
-                        Debug.LogError("Default Material \"" + defaultMaterialName + "\" not found. Please make sure the required Physic Materials are in a \"Resources\" folder.");
+                        Debug.LogWarning("Can't find Physic Material called \"" + physicMatName + "\" for Texture \"" + textureName + "\". Default Material \"" + defaultMaterialName + "\" used instead.");
+                        physicMat = (PhysicMaterial)Resources.Load(defaultMaterialName);
+                        if (physicMat != null) {
+                            sphereCollider.material = sphereCollider.material;
+                        } else {
+                            Debug.LogError("Default Material \"" + defaultMaterialName + "\" not found. Please make sure the required Physic Materials are in a \"Resources\" folder.");
+                        }
                     }
+                } else {
+                    Debug.LogWarning("Texture \"" + textureName + "\"'s name is too short. The start of the name should match a Physic Material in a \"Resources\" folder.");
                 }
             } else {
-                Debug.LogWarning("Texture \"" + textureName + "\"'s name is too short. The start of the name should match a Physic Material in a \"Resources\" folder.");
+                Debug.LogError("TerrainTextureDictionnary.Textures not set. Please make sure the Terrain \"" + collision.gameObject.name + "\" has a TerrainTextureDictionnary Component.");
             }
         }
     }
