0b61a60bf2eeef8f9dec69fc21d075df31629477
nickbaz <nicobaz010@live.ca>
2017-12-09 14:13:54 -0500

    Ajout selection map et retour au travail
    - Peut changer de map
    - Le menu in game est fonctionnel
    - La map dans l'accueil rotate
    - le menu in game se trouve dans un prefab dans chaque map

--- Assets/Scripts/HUD.cs
+++ Assets/Scripts/HUD.cs
@@ -9,11 +9,18 @@ public class HUD : MonoBehaviour {
     public Canvas CanvasOptionsJeu;
     public Canvas CanvasHUD;
     public Canvas CanvasAide;
+    private int starter;
+
+
+    void start() {
+        Cursor.visible = true;
+    }
 
     void Awake()
     {
         CanvasOptionsJeu.enabled = false;
         CanvasAide.enabled = false;
+
     }
 
     public void menu_principal()
@@ -23,7 +30,8 @@ public class HUD : MonoBehaviour {
 
     public void reset()
     {
-        SceneManager.LoadScene(1);
+        Scene scene = SceneManager.GetActiveScene();
+        SceneManager.LoadScene(scene.name);
     }
         
 
@@ -46,6 +54,23 @@ public class HUD : MonoBehaviour {
     {
         CanvasHUD.enabled = false;
         CanvasAide.enabled = true;
+        Debug.Log("!!!!!");
     }
 
+    public void ChangeMap() {
+
+        Scene scene = SceneManager.GetActiveScene();
+        if (scene.name == "UI_Test" || scene.name == "Plaine Italie")
+            Application.LoadLevel(2);
+        
+        else if (scene.name == "Montagne Lac" ) 
+            Application.LoadLevel(3);
+        
+        else if (scene.name == "Moyen Orient") 
+            Application.LoadLevel(4);
+        
+        else if (scene.name == "Désert") 
+            Application.LoadLevel(5);
+        
+    }
 }
